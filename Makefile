fce5a230772f12565641c961fc8f6eab95ad85b4
Wojciech Tatarski <w.tatarski93@wp.pl>
2018-01-13 14:29:05 +0100

    Started adding new interface

--- Makefile
+++ Makefile
@@ -11,14 +11,14 @@ CFLAGS =  -Wall -c -I.
 CPPFLAGS = -Wall $(CPPINCLUDE_DIRS)
 LFLAGS = -Wall
 
-CPPSOURCE =$(wildcard *.cpp)
+CPPSOURCE =$(wildcard main.cpp shader.cpp controls.cpp)
 CPPOUTFILE = path_finding
 CPPOBJS = $(CPPSOURCE:.cpp=.o)
 
 all: $(CPPOUTFILE)
 
 $(CPPOUTFILE): $(CPPOBJS)
-		$(CPP) $(CPPFLAGS) $(CPPOBJS) -o $(CPPOUTFILE) -lGL -lGLU -lglut -lglfw `pkg-config --libs opencv` $(CPPLIBS)
+		$(CPP) $(CPPFLAGS) $(CPPOBJS) -o $(CPPOUTFILE) -lGL -lGLU -lglut -lglfw -lGLEW `pkg-config --libs opencv` $(CPPLIBS)
 
 clean:
 		rm -f *.o $(CPPOUTFILE)
--- main.cpp
+++ main.cpp
@@ -1,39 +1,168 @@
-#include "scene.h"
-#include "pathfinding.h"
+#include <iostream>
+#include <vector>
+#include <opencv2/opencv.hpp>
+
+//#include "scene.h"
+//#include "pathfinding.h"
+#include "shader.hpp"
+#include "controls.hpp"
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+GLFWwindow* window;
+
 
 using namespace std;
 using namespace cv;
 
-float angle = 1.0f;
-
-// actual vector representing the camera's direction
-float lx=1.0f,lz=1.0f, ly = 0.0f;
-
-// XZ position of the camera
-float x=-1.0f, z=-1.0f, y = 1.75f;
-
-// the key states. These variables will be zero
-//when no key is being presses
-float deltaAngle = 0.0f;
-float deltaMove = 0;
-int xOrigin = -1;
-
-// width and height of the window
-int h,w;
-
-// variables to compute frames per second
-int frame;
-long time1, timebase;
-char s[50];
-
-// variables to hold window identifiers
-int mainWindow, subWindow1,subWindow2,subWindow3;
-//border between subwindows
-int border = 6;
 
 int main(int argc, char **argv) {
 
-        int menu=0;
+        /* Initialise GLFW */
+	if (!glfwInit()) {
+		fprintf( stderr, "Failed to initialize GLFW\n" );
+		getchar();
+		return -1;
+	}
+        /* Enable Multisample anti-aliasing (MSAA), 4* MSAA */
+	glfwWindowHint(GLFW_SAMPLES, 4);
+        /* Set API version to OpenGL 3.3 */
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	/* Open a window and create its OpenGL context */
+	window = glfwCreateWindow( 1024, 768, "Pathfinding", NULL, NULL);
+	if (window == NULL) {
+		fprintf( stderr, "Failed to open GLFW window. If you have an "
+                        "Intel GPU, they are not 3.3 compatible. Try the 2.1 "
+                        "version of the tutorials.\n" );
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	/* Initialize GLEW */
+        glewExperimental = true;
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		getchar();
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Ensure we can capture the escape key being pressed below */
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        glfwPollEvents();
+        glfwSetCursorPos(window, 1024/2, 768/2);
+
+	/* Dark blue background */
+	glClearColor(0.0f, 0.0f, 0.3f, 0.0f);
+
+        glEnable(GL_DEPTH_TEST);
+        glDepthFunc(GL_LESS);
+
+        GLuint axisBufferID;
+        glGenVertexArrays(1, &axisBufferID);
+        glBindVertexArray(axisBufferID);
+
+        GLuint programID = LoadShaders("SimpleTransform.vertexshader",
+                "ColorFragmentShader.fragmentshader");
+        GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+        //glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f,
+        //            0.1f, 100.0f);
+
+        // Camera matrix
+        /*glm::mat4 View = glm::lookAt(
+            glm::vec3(10,6,6), // Camera is at (4,3,3), in World Space
+            glm::vec3(0,0,0), // and looks at the origin
+            glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+            );
+        glm::mat4 Model = glm::mat4(1.0f);
+        glm::mat4 MVP = Projection * View * Model;
+*/
+        static const GLfloat g_vertex_buffer_data[] = {
+            0.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f,
+        };
+
+        static const GLfloat g_color_buffer_data[] = {
+            1.0f, 0.0f, 0.0f,
+            1.0f, 0.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
+            0.0f, 0.0f, 1.0f,
+            0.0f, 0.0f, 1.0f,
+        };
+
+        GLuint axisBuffer;
+        glGenBuffers(1, &axisBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, axisBuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
+                g_vertex_buffer_data, GL_STATIC_DRAW);
+
+        GLuint colorBuffer;
+        glGenBuffers(1, &colorBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data),
+                g_color_buffer_data, GL_STATIC_DRAW);
+
+	do{
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glUseProgram(programID);
+
+            computeMatricesFromInputs();
+            glm::mat4 ProjectionMatrix = getProjectionMatrix();
+            glm::mat4 ViewMatrix = getViewMatrix();
+            glm::mat4 ModelMatrix = glm::mat4(1.0);
+            glm::mat4 MVP = ProjectionMatrix * ViewMatrix * ModelMatrix;
+
+            glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+            glEnableVertexAttribArray(0);
+            glBindBuffer(GL_ARRAY_BUFFER, axisBuffer);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+            /* Color */
+            glEnableVertexAttribArray(1);
+            glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+
+            glDrawArrays(GL_LINES, 0, 6);
+
+            glDisableVertexAttribArray(0);
+            glDisableVertexAttribArray(1);
+
+            /* Swap buffers */
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+
+        } /* Check if the ESC key was pressed or the window was closed */
+        while(glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+                glfwWindowShouldClose(window) == 0 );
+
+        // Cleanup VBO
+	glDeleteBuffers(1, &axisBuffer);
+        glDeleteBuffers(1, &colorBuffer);
+	glDeleteVertexArrays(1, &axisBufferID);
+	glDeleteProgram(programID);
+
+	/* Close OpenGL window and terminate GLFW */
+	glfwTerminate();
+
+        return 0;
+        /*int menu=0;
         while(!(menu == 1 || menu ==2))
         {
             cout << "Wybierz scenariusz 1 lub 2 " << endl;
@@ -153,7 +282,7 @@ int main(int argc, char **argv) {
 	init();
 
 	// enter GLUT event processing cycle
-	glutMainLoop();
+	glutMainLoop();*/
 
 	return 0;
 }
